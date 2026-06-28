2e60bbf60f528214fba6210dad3f95e9cc372c5b
dheena <dheenad901@gmail.com>
2026-06-28 09:50:52 +0530

    Initializing the project and configure the MySql

---
+++ backend/src/test/java/com/dheena/tasktracker/TasktrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dheena.tasktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasktrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
